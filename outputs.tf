49e25509b49a01c66421f1bb982f9ffd81855367
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:11:24 +0100

    feat: implement tf-atom-ecs-cluster-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the ECS cluster"
+  value       = try(aws_ecs_cluster.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the ECS cluster"
+  value       = try(aws_ecs_cluster.this[0].arn, null)
+}
+
+output "name" {
+  description = "Name of the ECS cluster"
+  value       = try(aws_ecs_cluster.this[0].name, null)
 }
